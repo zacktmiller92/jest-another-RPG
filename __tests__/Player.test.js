d693b8cb72937c1289b5c4094be68681cf4c8878
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-19 08:07:31 -0400

    feature/player-object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -7,7 +7,7 @@ console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
-  
+
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
@@ -15,25 +15,55 @@ test('creates a player object', () => {
 
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
-      );
-  });
+    );
+});
 
-  test("gets player's stats as an object", () => {
+test("gets player's stats as an object", () => {
     const player = new Player('Dave');
-  
+
     expect(player.getStats()).toHaveProperty('potions');
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
-  });
+});
 
-  
-  test('gets inventory from player or returns false', () => {
+
+test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
-  
+
     expect(player.getInventory()).toEqual(expect.any(Array));
-  
+
     player.inventory = [];
-  
+
     expect(player.getInventory()).toEqual(false);
-  });
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
